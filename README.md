2f425737915a7b0cebd0cb0b164b6a66d778c66f
nazariyv <nazariy@inbox.ru>
2020-09-13 21:07:04 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AaveFlashloan
+
 Example of Aave Flashloan as per https://medium.com/coinmonks/flashloan-101-a35132a2bbf8
---
+++ contracts/Flashloan.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "./aave/FlashLoanReceiverBase.sol";
+import "./aave/ILendingPoolAddressesProvider.sol";
+import "./aave/ILendingPool.sol";
+
+contract Flashloan is FlashLoanReceiverBase {
+    constructor(address _addressesProvider)
+        public
+        FlashLoanReceiverBase(_addressesProvider)
+    {}
+
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address _reserve,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _params
+    ) external override {
+        require(
+            _amount <= getBalanceInternal(address(this), _reserve),
+            "Invalid balance, was the flashLoan successful?"
+        );
+
+        //
+        // Your logic goes here.
+        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
+        //
+
+        uint256 totalDebt = _amount.add(_fee);
+        transferFundsBackToPoolInternal(_reserve, totalDebt);
+    }
+
+    /**
+        Flash loan 1000000000000000000 wei (1 ether) worth of `_asset`
+     */
+    function flashloan(address _asset) public onlyOwner {
+        bytes memory data = "";
+        uint256 amount = 1 ether;
+
+        ILendingPool lendingPool = ILendingPool(
+            addressesProvider.getLendingPool()
+        );
+        lendingPool.flashLoan(address(this), _asset, amount, data);
+    }
+}
